ffee07df76cd7f5e3b85a19cb8607fa5b62bc6d1
balljul <code@juliusball.com>
2023-08-28 21:34:22 +0200

    tested a bit

--- app/main.py
+++ app/main.py
@@ -2,20 +2,21 @@ import argparse
 
 print('Docker Container is running')
 
-
+print(proc.stdout)
 # General Information
 
-parser = argparse.ArgumentParser(
-    prog='TaskLoggerX',
-    description='A dockerized Phyton CLI Tool to track and manage your worktime'
-)
+parser = argparse.ArgumentParser(prog='TaskLoggerX', description='A dockerized Phyton CLI Tool to track and manage your worktime')
 
 # Commands
 
-parser.add_argument(
-    'startDay',
-    action='store_true', 
-    help='Starts a new day'                
-)
+parser.add_argument('tlx')
+parser.add_argument('-s', '--start')
+parser.add_argument('-e', '--end')
+parser.add_argument('-d', '--description', action='store_true')
 
-print(parser.parse_args())
+args = parser.parse_args()
+print(args.tlx, args.start, args.end, args.description)
+
+if args.tlx == 'start':
+    print('Starting TaskLoggerX')
+
