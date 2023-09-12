924bb769dc17eb21ef32646c8664ca95bcd84c85
balljul <juliusball197@gmail.com>
2023-09-12 21:11:06 +0200

    UPDATED the argument evaluation

--- app/main.py
+++ app/main.py
@@ -1,51 +1,26 @@
-#import argparse
+import argparse
 
 # General Information
 
-#parser = argparse.ArgumentParser(prog='TaskLoggerX', description='A dockerized Phyton CLI Tool to track and manage your tasks')
+parser = argparse.ArgumentParser(prog='TaskLoggerX', description='A dockerized Phyton CLI Tool to track and manage your tasks')
 
 # Commands
 
-#parser.add_argument('tlx')
-#parser.add_argument('-s', '--start')
-#parser.add_argument('-e', '--end')
-#parser.add_argument('-d', '--description', action='store_true')
-
-#args = parser.parse_args()
-
-#if args.tlx == 'start':
- #   print('Starting TaskLoggerX')
-
-
-
-
-import argparse
-import datetime
-from pathlib import Path
-
-parser = argparse.ArgumentParser()
-
-parser.add_argument("path")
-parser.add_argument("-l", "--long", action="store_true")
+parser.add_argument('tlx')
+# parser.add_argument('tlx', choices=['start', 'end', 'description'], help='Available commands: start, end, description')
+parser.add_argument('-s', '--start', action='store_true')
+parser.add_argument('-e', '--end', action='store_true')
+parser.add_argument('-d', '--description', action='store_true')
 
 args = parser.parse_args()
 
-target_dir = Path(args.path)
-
-if not target_dir.exists():
-    print("The target directory doesn't exist")
-    raise SystemExit(1)
-
-def build_output(entry, long=False):
-    if long:
-        size = entry.stat().st_size
-        date = datetime.datetime.fromtimestamp(
-            entry.stat().st_mtime).strftime(
-            "%b %d %H:%M:%S"
-        )
-        return f"{size:>6d} {date} {entry.name}"
-    return entry.name
-
-
-for entry in target_dir.iterdir():
-    print(build_output(entry, long=args.long))
+if args.start == True and args.end == True:
+    print('Set start and end to true')
+elif args.start == True:
+   print('Set start to true')
+elif args.end == True:
+   print('Set end to true')
+elif args.description == True:
+    print("TaskLoggerX: A dockerized Phyton CLI Tool to track and manage your tasks")
+else:
+    print('No command was given')
