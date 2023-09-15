72d58e8c5a5270755058de0e3cc50f351a5ace86
balljul <juliusball197@gmail.com>
2023-09-15 22:11:08 +0200

    ADDED feature add a worktime writes it into a textfile

--- app/main.py
+++ app/main.py
@@ -6,9 +6,9 @@ parser = argparse.ArgumentParser(prog='TaskLoggerX', description='A dockerized P
 
 # Arguments
 parser.add_argument('tlx')
-parser.add_argument('-s', '--start', action='store_true')
-parser.add_argument('-e', '--end', action='store_true')
-parser.add_argument('-d', '--description', action='store_true')
+parser.add_argument('-s', '--start', dest='start', action='store')
+parser.add_argument('-e', '--end', dest='stop', action='store')
+parser.add_argument('-d', '--description', dest='desc', action='store_const')
 parser.add_argument('-ct', '--current-time', action='store_true', dest='show_time', help='Gives you the current CET and UTC Timestamp')
 
 args = parser.parse_args()
@@ -22,9 +22,26 @@ class Color:
 	YELLOW = '\033[93m'
 	RESET = '\033[0m'
 
+tf=open("worktime.txt","r+")
+
+timedata=tf.readlines()
+timedata_len=len(timedata)
+
 if args.show_time:
 	current_cet_timestamp = strftime("%a, %d %b %Y %H:%M:%S", localtime())
 	current_utc_timestamp = strftime("%a, %d %b %Y %H:%M:%S", gmtime())
 
 	print(f"{Color.GREEN}CET Timestamp:{Color.RESET} {current_cet_timestamp}")
 	print(f"{Color.YELLOW}UTC Timestamp:{Color.RESET} {current_utc_timestamp}")
+
+if args.start != None and args.stop != None:
+	tf.write(args.start + " | " + args.stop + "\n")
+elif args.start != None:
+	print(args.start)
+	print('Didn´t enter an end value')
+elif args.stop != None:
+	print(args.stop)
+	print("Didnt enter a start value")
+
+if args.desc:
+	print(timedata_len)
