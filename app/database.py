56af297b0dfaa94c733ebbd72a365c4614cba496
balljul <juliusball197@gmail.com>
2023-09-18 14:35:44 +0200

    UPDATED migrations

--- app/database.py
+++ app/database.py
@@ -13,24 +13,24 @@ def show_tables():
     tlx_cursor.execute("SHOW TABLES;")
     for x in tlx_cursor:
         print(x)
-    
+
 def drop_worktime_table():
     tlx_cursor.execute("DROP TABLE worktime")
-    print("Worktime Table dropped")  
-    
+    print("Worktime Table dropped")
+
 def migrate_database():
     tlx_cursor.execute("DROP TABLE IF EXISTS worktime")
     print("Worktime Table dropped")
-    tlx_cursor.execute("CREATE TABLE worktime (ID INT AUTO_INCREMENT PRIMARY KEY, starttime VARCHAR(255), endtime VARCHAR(255))")
+    tlx_cursor.execute("CREATE TABLE worktime (ID INT AUTO_INCREMENT PRIMARY KEY, starttime VARCHAR(255), endtime VARCHAR(255), description VARCHAR(255))")
     print("Freshly migrated")
 
 
-def submit_worktime(starttime, endtime):
+def submit_worktime(starttime, endtime, description):
     insert_statement = (
-	"INSERT INTO worktime (starttime, endtime)"
-	"VALUES (%s, %s)"
+	"INSERT INTO worktime (starttime, endtime, description)"
+	"VALUES (%s, %s, %s)"
 	)
-    data=(starttime, endtime)
+    data=(starttime, endtime, description)
     tlx_cursor.execute(insert_statement, data)
     tlxdb.commit()
     print("Succesfully saved worktime")
