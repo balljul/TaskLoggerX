80a0d55aab92456719b57578b3f8dfe7b77c85e9
balljul <juliusball197@gmail.com>
2023-09-18 13:31:44 +0200

    UPDATED -ct argument to update in realtime

--- app/tlx_clock.py
+++ app/tlx_clock.py
@@ -1,3 +1,4 @@
+import time
 from time import localtime, gmtime, strftime
 import tlx_cnf
 
@@ -8,5 +9,13 @@ def get_utc_timestamp():
     return strftime("%a, %d %b %Y %H:%M:%S", gmtime())
 
 def output_ct_timestamps():
-    print(f"{tlx_cnf.Color.GREEN}CET Timestamp:{tlx_cnf.Color.RESET} {get_cet_timestamp()}")
-    print(f"{tlx_cnf.Color.YELLOW}UTC Timestamp:{tlx_cnf.Color.RESET} {get_utc_timestamp()}")
+	while True:
+		print("\033c", end="")
+
+		print(f"{tlx_cnf.Color.GREEN}CET Timestamp:{tlx_cnf.Color.RESET} {get_cet_timestamp()}")
+		print(f"{tlx_cnf.Color.YELLOW}UTC Timestamp:{tlx_cnf.Color.RESET} {get_utc_timestamp()}")
+
+		time.sleep(0.5)
+
+if __name__ == "__main__":
+	output_ct_timestamps()
