61c850cd2d09c92b81a188503ae4d5909e2b643b
balljul <juliusball197@gmail.com>
2023-09-21 11:58:25 +0200

    ADDED task-arguments

--- app/arguments.py
+++ app/arguments.py
@@ -10,6 +10,9 @@ parser.add_argument('-e', '--end', dest='stop', action='store', type=int)
 parser.add_argument('-ct', '--current-time', action='store_true', dest='show_time', help='Gives you the current CET and UTC Timestamp')
 parser.add_argument('-d', '--description', action="store", dest='description', help='Lets you add ')
 
+parser.add_argument('-tc', '--task-create', action='store_true', dest='task_create', help='Create a task')
+parser.add_argument('-n', '--name', action='store', dest='name', help='Set a name')
+
 parser.add_argument('-dbs', '--database-show', action='store_true', dest='dbshow',  help='Shows all tables in the database')
 parser.add_argument('--database-migrate', action='store_true', dest='dbmigrate', help='Migrates the database')
 parser.add_argument('--database-drop', action='store_true', dest='dbdrop', help='A quick way to drop all tables in the database')
