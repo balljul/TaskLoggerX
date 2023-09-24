ea4a9b3bf8fef6459d9110b6ba1bbe59cc03a97e
balljul <juliusball197@gmail.com>
2023-09-24 12:49:45 +0200

    ADDED feature append a task to worktime

--- app/database.py
+++ app/database.py
@@ -1,4 +1,4 @@
-import mysql.connector
+mport mysql.connector
 from tlx_cnf import db_cnf, Color
 from datetime import datetime, timedelta
 
@@ -17,17 +17,32 @@ class taskLoggerDb:
 	    for x in self.cursor:
 	        print(x)
 
-	def submit_worktime(self, start, stop, description):
+	def submit_worktime(self, start, stop, description, task):
 		insert_statement = (
 		"INSERT INTO worktime (starttime, endtime, description)"
 		"VALUES (%s, %s, %s)"
 		)
+		insert_statement_task = (
+		"INSERT INTO worktime_task (worktime_id, task_id)"
+		"VALUES (%s, %s)"
+		)
+		get_task_id_statement = (
+		"SELECT id FROM task WHERE name = %s"
+		)
 
+		# worktime table
 		starttime = datetime.now().replace(hour = start, minute = 0, second = 0)
 		endtime = datetime.now().replace(hour = stop, minute = 0, second = 0)
-
 		data=(starttime, endtime, description)
 		self.cursor.execute(insert_statement, data)
+		self.conn.commit()
+		worktime_id = self.cursor.lastrowid
+
+		# worktime_task table
+		self.cursor.execute(get_task_id_statement, (task,))
+		task_id = self.cursor.fetchone()
+
+		self.cursor.execute(insert_statement_task, (worktime_id, task_id[0]))
 		self.conn.commit()
 		print("Succesfully saved worktime")
 
@@ -42,12 +57,21 @@ class taskLoggerDb:
 		self.conn.commit()
 		print("Succesfully created task")
 
+	def check_task_input(self, input):
+		query = (
+		"SELECT * FROM task"
+		)
+		data = self.cursor.execute(query)
+
+		print(data)
+
+
 	def list_task(self):
 		query = (
 		"SELECT * FROM task"
 		)
-
 		self.cursor.execute(query)
+
 		for i in self.cursor:
 			if i[2] != None:
 				print(f"{Color.RED}ID: {Color.RESET}{i[0]}  {Color.GREEN}Name: {Color.RESET}{i[1]}  {Color.YELLOW}Description: {Color.RESET}{i[2]} " + "\n")
